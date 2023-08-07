1da932db0887301155ebfefa8743e54d966ec6db
Damandeep Singh <73746851+Damandeep02@users.noreply.github.com>
2023-08-07 16:13:20 +0530

    Added new column

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../db'); 
+const User = require('./user');
 
 const Restaurant = sequelize.define('Restaurant', {
   name: {
@@ -18,7 +19,11 @@ const Restaurant = sequelize.define('Restaurant', {
     type: DataTypes.STRING, 
     allowNull: true, 
   },
+  added_by: {
+    type: DataTypes.INTEGER, 
+    allowNull: false, 
+  }
 
 });
-
+Restaurant.belongsTo(User, { foreignKey: 'added_by' });
 module.exports = Restaurant;
